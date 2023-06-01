cdb51591f71ecbb12a6fff584087e5b324ac413d
HX-TPiraino <tpiraino@soyhenry.com>
2023-06-01 19:51:55 -0300

    Ej 11 Extra terminado

--- src/intro_java/Extras_Intro_11.java
+++ src/intro_java/Extras_Intro_11.java
@@ -4,13 +4,29 @@
  * and open the template in the editor.
  */
 package intro_java;
-
+import java.util.Scanner;
 /**
  *
  * @author tpira
  */
 public class Extras_Intro_11 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int myNumber;
+        int aux=0;
         
+        System.out.println("Ingrese un numero");
+        myNumber = scanner.nextInt();
+        
+        // 1231 / 10 -> 103 / 10 -> 10 / 10 -> 1 / 10 -> 0
+        
+        while(true){
+          myNumber = (int)(Math.round(myNumber / 10));
+          aux++;
+          if(myNumber == 0){
+              System.out.println("Tiene " + aux + " cifras");
+              break;
+          }
+        }
     }
 }
