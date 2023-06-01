c18f3ba7cc6a96b87e5f66ba23d66912b71b88df
HX-TPiraino <tpiraino@soyhenry.com>
2023-06-01 20:31:54 -0300

    Ej 21 fibonacci echo

--- src/intro_java/Extras_Intro_24.java
+++ src/intro_java/Extras_Intro_24.java
@@ -5,12 +5,24 @@
  */
 package intro_java;
 
+import java.util.Arrays;
+
 /**
  *
  * @author tpira
  */
 public class Extras_Intro_24 {
     public static void main(String[] args) {
+        int n = 10;
+        int[] fibonacci = new int[n];
+        fibonacci[0] = 1;
+        fibonacci[1] = 1;
+        fibonacci[2] = 2;
         
+        for (int i = 3; i < n; i++) {
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        }
+        
+        System.out.println(Arrays.toString(fibonacci));
     }
 }
